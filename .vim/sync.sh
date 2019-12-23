0dfcac15a40706c930234ec11e0718af82f12c82
Daniel <daniel.zitzman@gig.com>
2019-12-23 16:17:31 +0100

    Move to nvim config dir

--- .vim/sync.sh
+++ .vim/sync.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mkdir -p ~/.vim/undo/ ~/.vim/spell ~/.vim/autoload
+mkdir -p ~/.vim/undo/ ~/.vim/spell
 
 if [ ! -f ~/.vim/autoload/plug.vim ]; then
     curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
