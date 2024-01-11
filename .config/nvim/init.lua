5ed52dc6231a0b78c35018c7feec173ecad129fa
Daniel Zitzman <daniel.zitzman@betssongroup.com>
2024-01-11 18:27:40 +0100

    updates

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -404,13 +404,17 @@ require("lazy").setup({
 
   { -- Fuzzy Search
     'nvim-telescope/telescope.nvim',
-    dependencies = { { 'nvim-lua/plenary.nvim' } },
+    dependencies = {
+      { 'nvim-lua/plenary.nvim' },
+      { "nvim-telescope/telescope-live-grep-args.nvim" },
+    },
     config = function()
+      require("telescope").load_extension("live_grep_args")
       local builtin = require('telescope.builtin')
       vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
       vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
       vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+      vim.keymap.set("n", "<leader>fg", ":lua require('telescope').extensions.live_grep_args.live_grep_args()<CR>")
     end
   },
 
