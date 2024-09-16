48cc44d5550fd7cdb2581ae284bc6d3b73e75848
Daniel Zitzman <daniel.zitzman@betssongroup.com>
2024-09-16 15:31:52 +0200

    Update init.lua

--- .config/nvim/after/plugin/copilot.lua
+++
@@ -1,4 +0,0 @@
-vim.g.copilot_no_tab_map = true
-vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
-vim.api.nvim_set_keymap('i', '<C-S>', '<Plug>(copilot-next)', { noremap = false })
-vim.api.nvim_set_keymap('i', '<C-A>', '<Plug>(copilot-previous)', { noremap = false })
--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,3 +1,344 @@
 require('globals.options')
 require('globals.remaps')
-require('plugins.packer')
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+-- Example using a list of specs with the default options
+vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+
+require("lazy").setup({
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+    init = function()
+      vim.o.timeout = true
+      vim.o.timeoutlen = 300
+    end,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    }
+  },
+
+  { "folke/neoconf.nvim",      cmd = "Neoconf" },
+  "folke/neodev.nvim",
+  { "lewis6991/impatient.nvim" },
+  { "nathom/filetype.nvim" },
+
+  'tpope/vim-characterize',
+  'tpope/vim-commentary',
+  {
+    'tpope/vim-fugitive',
+    config = function()
+      vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+    end
+  },
+
+  {
+    'github/copilot.vim',
+    config = function()
+      vim.g.copilot_no_tab_map = true
+      vim.api.nvim_set_keymap("i", "<C-J>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
+      vim.api.nvim_set_keymap('i', '<C-S>', '<Plug>(copilot-next)', { noremap = false })
+      vim.api.nvim_set_keymap('i', '<C-A>', '<Plug>(copilot-previous)', { noremap = false })
+    end
+  },
+
+  -- {
+  --   -- Local plugin path
+  --   dir = '~/projects/neovim-gpt-agents',
+  --   config = function()
+  --   end
+  -- },
+
+  { -- The ultimate undo history visualizer for VIM
+    'mbbill/undotree',
+    config = function()
+      vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+      vim.g.undotree_WindowLayout = 2
+      vim.g.undotree_SplitWidth = 35
+      vim.g.undotree_SetFocusWhenToggle = 1
+      vim.g.undotree_TreeNodeShape = '◉'
+      vim.g.undotree_DiffAutoOpen = 0
+    end
+  },
+
+  'tpope/vim-obsession',
+  'tpope/vim-projectionist',
+  'tpope/vim-ragtag',
+  'tpope/vim-repeat',
+  'tpope/vim-sensible',
+  'tpope/vim-surround',
+  'tpope/vim-unimpaired',
+  'tpope/vim-vinegar',
+
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup()
+    end,
+  },
+
+  'gpanders/editorconfig.nvim', -- EditorConfig plugin for Vim
+
+  {
+    'nvim-lualine/lualine.nvim', -- A blazing fast Neovim statusline
+    dependencies = { 'kyazdani42/nvim-web-devicons', opt = true },
+    config = function()
+      require('lualine').setup()
+    end
+  },
+
+  {
+    "ellisonleao/gruvbox.nvim",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      -- load the colorscheme here
+      vim.o.background = "dark" -- or "light" for light mode
+      vim.cmd([[colorscheme gruvbox]])
+    end,
+  },
+
+  { -- A super powerful autopair plugin for Neovim that supports multiple characters
+    "windwp/nvim-autopairs",
+    config = function() require("nvim-autopairs").setup {} end
+  },
+
+  { -- auto-save
+    "Pocco81/auto-save.nvim",
+    config = function()
+      require("auto-save").setup {
+        -- your config goes here
+        -- or just leave it empty :)
+      }
+    end,
+  },
+
+  { -- Markdown preview plugin
+    'toppair/peek.nvim',
+    build = 'deno task --quiet build:fast',
+    config = function()
+      -- Markdown preview
+      vim.api.nvim_create_user_command('PeekOpen', require('peek').open, {})
+      vim.api.nvim_create_user_command('PeekClose', require('peek').close, {})
+    end,
+  },
+
+  {
+    'nathanaelkane/vim-indent-guides',
+    lazy = false,
+    config = function()
+      vim.g.indent_guides_enable_on_vim_startup = 1
+      vim.g.indent_guides_auto_colors = 0
+    end
+  },
+
+  { -- Fuzzy Search
+    'nvim-telescope/telescope.nvim',
+    dependencies = { { 'nvim-lua/plenary.nvim' } },
+    config = function()
+      local builtin = require('telescope.builtin')
+      vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
+      vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
+      vim.keymap.set('n', '<leader>fb', builtin.buffers, {})
+      vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
+    end
+  },
+
+  {
+    "nvim-treesitter/nvim-treesitter",
+    version = false,
+    build = ":TSUpdate",
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = { "TSUpdateSync" },
+    dependencies = {
+      {
+        "nvim-treesitter/nvim-treesitter-textobjects",
+        init = function()
+          -- PERF: no need to load the plugin, if we only need its queries for mini.ai
+          local plugin = require("lazy.core.config").spec.plugins["nvim-treesitter"]
+          local opts = require("lazy.core.plugin").values(plugin, "opts", false)
+          local enabled = false
+          if opts.textobjects then
+            for _, mod in ipairs({ "move", "select", "swap", "lsp_interop" }) do
+              if opts.textobjects[mod] and opts.textobjects[mod].enable then
+                enabled = true
+                break
+              end
+            end
+          end
+          if not enabled then
+            require("lazy.core.loader").disable_rtp_plugin("nvim-treesitter-textobjects")
+          end
+        end,
+      },
+    },
+    keys = {
+      { "<c-space>", desc = "Increment selection" },
+      { "<bs>",      desc = "Decrement selection", mode = "x" },
+    },
+    ---@type TSConfig
+    opts = {
+      highlight = { enable = true },
+      indent = { enable = true },
+      ensure_installed = {
+        "bash",
+        "c",
+        "html",
+        "javascript",
+        "json",
+        "lua",
+        "luadoc",
+        "luap",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "tsx",
+        "typescript",
+        "vim",
+        "vimdoc",
+        "yaml",
+      },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<C-space>",
+          node_incremental = "<C-space>",
+          scope_incremental = false,
+          node_decremental = "<bs>",
+        },
+      },
+    },
+    ---@param opts TSConfig
+    config = function(_, opts)
+      if type(opts.ensure_installed) == "table" then
+        ---@type table<string, boolean>
+        local added = {}
+        opts.ensure_installed = vim.tbl_filter(function(lang)
+          if added[lang] then
+            return false
+          end
+          added[lang] = true
+          return true
+        end, opts.ensure_installed)
+      end
+      require("nvim-treesitter.configs").setup(opts)
+    end,
+
+  },
+
+  {
+    {
+      'VonHeikemen/lsp-zero.nvim',
+      branch = 'v4.x',
+      lazy = true,
+      config = false,
+    },
+    {
+      'williamboman/mason.nvim',
+      lazy = false,
+      config = true,
+    },
+
+    -- Autocompletion
+    {
+      'hrsh7th/nvim-cmp',
+      event = 'InsertEnter',
+      dependencies = {
+        { 'L3MON4D3/LuaSnip' },
+      },
+      config = function()
+        local cmp = require('cmp')
+
+        cmp.setup({
+          sources = {
+            { name = 'nvim_lsp' },
+          },
+          mapping = cmp.mapping.preset.insert({
+            ['<C-Space>'] = cmp.mapping.complete(),
+            ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+            ['<C-d>'] = cmp.mapping.scroll_docs(4),
+          }),
+          snippet = {
+            expand = function(args)
+              vim.snippet.expand(args.body)
+            end,
+          },
+        })
+      end
+    },
+
+    { -- LSP
+      'neovim/nvim-lspconfig',
+      cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
+      event = { 'BufReadPre', 'BufNewFile' },
+      dependencies = {
+        { 'hrsh7th/cmp-nvim-lsp' },
+        { 'williamboman/mason.nvim' },
+        { 'williamboman/mason-lspconfig.nvim' },
+      },
+      config = function()
+        local lsp_zero = require('lsp-zero')
+
+        -- lsp_attach is where you enable features that only work
+        -- if there is a language server active in the file
+        local lsp_attach = function(client, bufnr)
+          local opts = { buffer = bufnr }
+
+          vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+          vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+          vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+          vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+          vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+          vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+          vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+          vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+          vim.keymap.set({ 'n', 'x' }, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+          vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+        end
+
+        lsp_zero.extend_lspconfig({
+          sign_text = true,
+          lsp_attach = lsp_attach,
+          capabilities = require('cmp_nvim_lsp').default_capabilities()
+        })
+
+        require('mason-lspconfig').setup({
+          ensure_installed = {
+            'tsserver',
+            'eslint',
+            'lua_ls',
+            'solargraph',
+            'intelephense',
+            'marksman',
+            'dockerls',
+            'bashls',
+          },
+          handlers = {
+            -- this first function is the "default handler"
+            -- it applies to every language server without a "custom handler"
+            function(server_name)
+              require('lspconfig')[server_name].setup({})
+            end,
+          }
+        })
+      end
+    }
+  },
+
+})
